27d4ca8720069ad086386b1fc324f739639e4100
BrandonCorn <cornbrandonk@gmail.com>
2024-05-18 15:12:44 -0400

    modify add new pet form, update fetching of customer to allow for suspense correctly

---
+++ actions/pet.tsx
@@ -0,0 +1,4 @@
+'use server';
+import z from 'zod';
+
+export default async function addNewPet(state: any, formData: FormData) {}
--- lib/schema.ts
+++ lib/schema.ts
@@ -80,7 +80,7 @@ export const customers = pgTable("customer",
 )
 
 export const customerRelations = relations(customers, ({ many }) => ({
-  customersToPets: many(customersToPets),
+  pets: many(customersToPets),
 }))
 
 export type Customers = {
@@ -108,27 +108,28 @@ export const pet = pgTable("pet",
     color: text('color').notNull(),
     age: integer('age').notNull(),
     fixed: text('fixed').notNull(),
+    owner: text('owner').references(() => customers.email),
     createdAt: timestamp('createdAt', { mode: 'date'}).$defaultFn(() => new Date()),
     updatedAt: timestamp('updatedAt', { mode: 'date'}).$defaultFn(() => new Date()),
   }
 )
 
 export const petRelations = relations(pet, ({ many }) => ({
-
+  owners: many(customersToPets)
 }))
 
-// export type Pets = {
-//   id: string;
-//   name: string;
-//   type: 'dog' | 'cat';
-//   breed: string;
-//   gender: string;
-//   weight: string;
-//   color: string;
-//   age: string;
-//   createdAt: Date;
-//   updatedAt: Date;
-// }
+export type Pets = {
+  id: string;
+  name: string;
+  type: 'dog' | 'cat';
+  breed: string;
+  gender: string;
+  weight: string;
+  color: string;
+  age: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export const customersToPets = pgTable(
   'customers_to_pets',
